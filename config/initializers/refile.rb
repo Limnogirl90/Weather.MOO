029614b21351d282924aec463c6ea18c73c42838
Kingdon Barrett <kbarrett@metrixmatrix.com>
2016-08-14 17:59:03 -0400

    this works with S3
    setting the AWS_REGION env variable seems to wreak some havoc with
    refile-s3, so don't do that... just pass a region into s3_options

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,13 +1,10 @@
 require 'refile/s3'
 
-#aws = {
-#  region:            'us-east-1',
-#  access_key_id:     ENV['S3_ACCESS_KEY'],
-#  secret_access_key: ENV['S3_SECRET_KEY'],
-#  bucket:            ENV['S3_BUCKET_NAME'],
-##}
-#Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
-#Refile.store = Refile::S3.new(prefix: 'store', **aws)
-
-Refile.cache = Refile::Backend::FileSystem.new("tmp/refile/cache")
-Refile.store = Refile::Backend::FileSystem.new("tmp/refile/store")
+aws = {
+  region:            'us-east-1',
+  access_key_id:     ENV['S3_ACCESS_KEY'],
+  secret_access_key: ENV['S3_SECRET_KEY'],
+  bucket:            ENV['S3_BUCKET_NAME'],
+}
+Refile.cache = Refile::S3.new(region: ENV['AWS_REGION'], prefix: 'cache', **aws)
+Refile.store = Refile::S3.new(region: ENV['AWS_REGION'], prefix: 'store', **aws)
