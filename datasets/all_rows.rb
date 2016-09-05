225d789d51ff1415b802c9fdc3304bd98d00faf3
Kingdon Barrett <kbarrett@metrixmatrix.com>
2016-09-05 19:14:49 -0400

    cleanup debugging statements

--- datasets/all_rows.rb
+++ datasets/all_rows.rb
@@ -10,19 +10,15 @@ WeatherDatum.all.each do |w|
   else
     d = w.observation_date
   end
-  puts "caching weather data for #{d} locally"
   WeatherDatum.transaction do
     if w.weather_html_url.nil?
       url = WeatherDatum.weather_url_from_date(d)
-      puts "updating URL field"
       w.weather_html_url = url
     end
     if w.observation_date.nil?
-      puts "updating observation_date field"
       w.observation_date = d
     end
     if w.csv_row.nil?
-      puts "saving CSV data"
       w.csv_row = Marshal.dump(w.weather_data_row)
     end
     w.save
@@ -37,9 +33,7 @@ WeatherDatum.all.each do |w|
   end
   rows << csv_row
 end
-#STDERR.puts "printing out a big old CSV"
 puts header.to_csv
 rows.each do |row|
   puts header.map {|h| row[h]}.to_csv
 end
-#puts csv_table
